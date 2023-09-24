8df7a88d57ec758ec9706da2f3008fbe4213bcec
sj0000967 <145891308+sj0000967@users.noreply.github.com>
2023-09-24 01:24:25 -0500

    Delete spingcloud/spingcloud directory

--- spingcloud/spingcloud/employee-service/src/main/java/com/sahan/employeeservice/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sahan.employeeservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sahan.employeeservice.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    
-}
